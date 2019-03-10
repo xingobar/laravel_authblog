3efce97e435002c359b96dc5fc021ca47d95c281
xingobar <gary840227@gmail.com>
2019-03-10 13:13:45 +0800

    1. change avatar type in user table

---
+++ database/migrations/2019_03_10_050554_change_user_avatar_type.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeUserAvatarType extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function ($table) {
+            $table->string('avatar')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function ($table) {
+            $table->string('avatar')->change();
+        });
+    }
+}
